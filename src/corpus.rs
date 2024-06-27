1a4549cc07dd8ed05fc15d4614d94c272f641a3e
Anastasiya Polina Soyka <apsoyka@protonmail.com>
2024-06-27 18:29:25 -0400

    Merge Corpora Into Single Compressed Corpus File

--- src/corpus.rs
+++ src/corpus.rs
@@ -1,7 +1,7 @@
 use std::{fs::read, path::PathBuf, str::from_utf8};
 
-use include_dir::{include_dir, Dir};
 use log::debug;
+use zstd::bulk::decompress;
 
 type BoxedError<'a> = Box<dyn std::error::Error + Send + Sync + 'a>;
 type VecStringResult<'a> = Result<Vec<String>, BoxedError<'a>>;
@@ -28,15 +28,11 @@ pub fn load_corpus<'a>(path: &PathBuf) -> VecStringResult<'a> {
     Ok(corpus)
 }
 
-static CORPORA: Dir = include_dir!("src/corpora");
-
 pub fn load_default_corpus<'a>() -> VecStringResult<'a> {
-    let mut corpus = CORPORA.files()
-        .map(|file| file.contents_utf8())
-        .flatten()
-        .map(|value| split_text(value, "\n"))
-        .flatten()
-        .collect::<Vec<_>>();
+    let buffer = include_bytes!("corpus.txt.zst");
+    let bytes = decompress(buffer, 180746)?;
+    let string = from_utf8(&bytes)?;
+    let mut corpus = split_text(string, "\n");
     let count = corpus.len();
 
     corpus.sort();
@@ -55,6 +51,6 @@ mod tests {
         let corpus = load_default_corpus().unwrap();
         let length = corpus.len();
 
-        assert_eq!(length, 19726);
+        assert_eq!(length, 18379);
     }
 }
